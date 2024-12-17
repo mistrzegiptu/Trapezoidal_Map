3c5f5edddd399ff5521c811a4edd2dedc5160069
vimzoomer <pelczarskik@student.agh.edu.pl>
2024-12-17 01:37:06 +0100

    initial commit

---
+++ project/main.py
@@ -0,0 +1,7 @@
+from visualizer.main import Visualizer
+
+vis = Visualizer()
+vis.add_point((1, 1))
+vis.show()
+
+
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+with open('requirements.txt') as f:
+    requirements = f.read().split('\n')
+
+setup(
+    name='project',
+    description='Laboratory classes in the Geometric Algorithms course for Computer Science students at the AGH University.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='AGH BIT Student Scientific Group',
+    packages=['project'],
+    python_requires='>=3.8',
+    install_requires=requirements,
+)
