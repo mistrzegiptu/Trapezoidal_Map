e3e689610bd0b7d09f2055b6020f7a5168464085
mistrzegiptu <mistrzegiptu@gmail.com>
2024-12-26 14:55:43 +0100

    Finished follow segment

--- project/src/trapezoidal_map.py
+++ project/src/trapezoidal_map.py
@@ -8,11 +8,31 @@ class TrapezoidalMap:
         self.segments = self._create_segments(permuted_s)
         self.rect_bound = self._create_rect_bound()
         self.tree = DTree()
+        self.tree.root = self.rect_bound
+
 
 
     def build_trapezoidal_map(self):
-        # TODO tomorrow
+        for i in range(len(self.segments)):
+            intersected_trapezoids = self.follow_segment(self.segments[i])
+
         return 0
+
+    def follow_segment(self, s: Segment):
+        p, q = s.to_tuple()
+        intersected_trapezoids = []
+        first_trapezoid = self.tree.find(self.tree.root, p, s.get_a())
+        intersected_trapezoids.append(first_trapezoid)
+
+        j = 0
+        while intersected_trapezoids[j].right < q:
+            if s.is_above(intersected_trapezoids[j].right):
+                intersected_trapezoids.append(intersected_trapezoids[j].bottom_right)
+            else:
+                intersected_trapezoids.append(intersected_trapezoids[j].top_right)
+            j += 1
+
+        return intersected_trapezoids
 
     def _create_segments(self, permuted_s) -> list[Segment]:
         result = []
