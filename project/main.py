0aca62c451c5b34b67bc4db9f5b73caebf867fa0
vimzoomer <pelczarskik@student.agh.edu.pl>
2024-12-28 21:41:18 +0100

    Refektoryzacja podziału jednego trapezu na trapezy

--- project/main.py
+++
@@ -1,8 +0,0 @@
-from visualizer.main import Visualizer
-from src.data_structures import *
-
-if __name__ == "__main__":
-    t = Trapezoid(Point(1, 1), Point(2, 1), Segment(Point(1, 1), Point(2, 1)), Segment(Point(1, 2), Point(2, 2)))
-    vis = Visualizer()
-    vis.add_line_segment(t.get_segments(as_tuples=True))
-    vis.show()
